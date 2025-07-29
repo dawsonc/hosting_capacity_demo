4868542cf0d083e55b9d0c6e3371eb153fa51e62
Charles Dawson <charles.dwsn@gmail.com>
2025-07-29 11:02:19 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -255,10 +255,10 @@ def plot_network(net: pp.pandapowerNet, violations: dict[str, pd.Series], pv_bus
                 y=[center_y],
                 mode="markers",
                 marker=dict(
-                    size=28,
+                    size=36,
                     color="rgba(0,0,0,1)",  # Fully transparent
                     line=dict(width=0),
-                    symbol="s",
+                    symbol="square",
                 ),
                 hovertemplate=hover_text,
                 showlegend=False,
