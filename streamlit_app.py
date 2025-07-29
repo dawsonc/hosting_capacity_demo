aa04339c37a5b4736a84ff8f0cc91c165862f471
Charles Dawson <charles.dwsn@gmail.com>
2025-07-29 10:15:20 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -129,6 +129,7 @@ def main() -> None:
     net = copy.deepcopy(get_base_network())
     st.sidebar.header("Study parameters")
     print(net.bus.geo)
+    print(net.bus_geodata)
     pv_bus = st.sidebar.selectbox("PV connection bus", options=list(net.bus.index), index=5)
     pv_kw = st.sidebar.slider("PV export capacity [kW]", 0, 500, value=0, step=10)
     st.sidebar.write(f"**PV @ bus {pv_bus}:** {pv_kw} kW")
